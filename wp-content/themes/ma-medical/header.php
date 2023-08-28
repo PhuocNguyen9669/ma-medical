cbdbe66439202654ef4a66900508d32d5d73b720
Phuoc Nguyen <PhuocNguyen.081096@gmail.com>
2023-08-28 13:59:57 +0700

    cm

--- wp-content/themes/ma-medical/header.php
+++ wp-content/themes/ma-medical/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta name="viewport" content="width=device-width">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title><?php the_title(); ?></title>
+    <title><?php the_title() ?></title>
     <meta name="description" content=" content " />
     <meta name="keywords" content=" content " />
     <meta name="author" content=" content " />
